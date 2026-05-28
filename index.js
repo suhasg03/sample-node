55b8f2132c863ca6dac0993582b65ebd1c0122d2
Suhas <suhas.03su@gmail.com>
2026-05-28 14:54:22 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+
+app.get("/", (req, res) => {
+    return res.send("Hello World");
+})
+
+app.listen(8080, () => {
+    console.log('Server is running on port 3000');
+})
